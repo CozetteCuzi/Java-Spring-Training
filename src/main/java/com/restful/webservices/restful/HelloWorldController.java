593198aed33d8b346353b828317f7194d093cb70
cozette.cuzi <cozette.cuzi@gmail.com>
2022-01-28 20:41:09 +0100

    Create HelloWorldController.java

---
+++ src/main/java/com/restful/webservices/restful/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.restful.webservices.restful;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// We tell spring that this is a controller which means it will deal with HTTP requests
+@RestController
+public class HelloWorldController {
+	// Get /hello-world
+//	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello Wrold";
+	}
+}
