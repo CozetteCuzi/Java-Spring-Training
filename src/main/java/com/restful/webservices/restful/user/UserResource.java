805472e8f81c8246697663cf242cd43784c4075e
cozette.cuzi <cozette.cuzi@gmail.com>
2022-01-30 00:35:24 +0100

    post a new user

--- src/main/java/com/restful/webservices/restful/user/UserResource.java
+++ src/main/java/com/restful/webservices/restful/user/UserResource.java
@@ -1,11 +1,16 @@
 package com.restful.webservices.restful.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -22,4 +27,15 @@ public class UserResource {
 	public User retreiveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
+	
+	@PostMapping(path="/users")
+	public ResponseEntity<Object> saveUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId())
+		.toUri();
+		return ResponseEntity.created(location).build();
+	}
+
 }
