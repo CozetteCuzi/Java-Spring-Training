5479886aacb78d7f199dc5c4d75048baa2ffe771
cozette.cuzi <cozette.cuzi@gmail.com>
2022-02-09 21:24:40 +0100

    enhancements on Internationalization

--- src/main/java/com/restful/webservices/restful/RestfulApplication.java
+++ src/main/java/com/restful/webservices/restful/RestfulApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 @SpringBootApplication
 public class RestfulApplication {
 
@@ -19,15 +18,9 @@ public class RestfulApplication {
 
 	@Bean
 	public LocaleResolver localResolver() {
-		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localResolver = new AcceptHeaderLocaleResolver();
 		localResolver.setDefaultLocale(Locale.US);
 		return localResolver;
 	}
 	
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("messages");
-		return messageSource;
-	}
 }
