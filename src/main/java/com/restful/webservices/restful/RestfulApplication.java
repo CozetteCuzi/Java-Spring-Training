ca7f3759fdaa127e42c6747040f74965cd923907
cozette.cuzi <cozette.cuzi@gmail.com>
2022-02-09 21:17:11 +0100

    Internationalization

--- src/main/java/com/restful/webservices/restful/RestfulApplication.java
+++ src/main/java/com/restful/webservices/restful/RestfulApplication.java
@@ -1,7 +1,14 @@
 package com.restful.webservices.restful;
 
+import java.util.Locale;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulApplication {
@@ -10,4 +17,17 @@ public class RestfulApplication {
 		SpringApplication.run(RestfulApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localResolver = new SessionLocaleResolver();
+		localResolver.setDefaultLocale(Locale.US);
+		return localResolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
--- src/main/java/com/restful/webservices/restful/helloworld/HelloWorldController.java
+++ src/main/java/com/restful/webservices/restful/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.restful.webservices.restful.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 // We tell spring that this is a controller which means it will deal with HTTP requests
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	// Get /hello-world
 //	@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	@GetMapping(path="/hello-world")
@@ -22,6 +30,11 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean/path-variable/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path="/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-language") Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 	
 }
